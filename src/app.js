81372485ed770c07ab903f5a4beea671d2a590ec
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2022-06-13 19:58:37 -0400

    Update cell size for performance using windowWidth

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ let mutant;
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('game-canvas');
-  w = 8;
+  w = windowWidth * 0.005;
   // Calculate columns and rows
   columns = floor(windowWidth / w);
   rows = floor(windowHeight / w);
