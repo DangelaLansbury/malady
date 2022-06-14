4e46f8b019a5b75e9bc3689d0c2de89060cb792a
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2022-06-13 20:12:02 -0400

    Testing new w value

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ let mutant;
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('game-canvas');
-  w = windowWidth * 0.005;
+  w = windowWidth * 0.00525;
   // Calculate columns and rows
   columns = floor(windowWidth / w);
   rows = floor(windowHeight / w);
