fd361890f7cec09360515e475ec988924119a3e8
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2022-06-15 07:35:08 -0400

    Up framerate one more time

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ let mutant;
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('game-canvas');
-  frameRate(18);
+  frameRate(20);
   w = windowWidth * 0.00525;
   // Calculate columns and rows
   columns = floor(windowWidth / w);
